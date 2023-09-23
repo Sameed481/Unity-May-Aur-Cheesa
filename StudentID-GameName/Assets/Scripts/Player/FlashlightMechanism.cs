34f703a123950039be3083b6412b35907bf7bcea
Muhammad Ramish <mramish9696@gmail.com>
2023-09-23 21:08:25 +0500

    - enemy sight range updates with flashlight status

--- StudentID-GameName/Assets/Scripts/Player/FlashlightMechanism.cs
+++ StudentID-GameName/Assets/Scripts/Player/FlashlightMechanism.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class FlashlightMechanism : MonoBehaviour
 
-    
+
 {
     public GameObject flashlight;
 
     public AudioSource turnOn;
     public AudioSource turnOff;
 
-   
-    public bool isFlashlightOn=false;
+    public GameObject enemyMovement;
+
+
+    public bool isFlashlightOn = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,14 +22,31 @@ public class FlashlightMechanism : MonoBehaviour
         flashlight.SetActive(false);
     }
 
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Flashlight"))
         {
-
             isFlashlightOn = !isFlashlightOn;
             flashlight.SetActive(isFlashlightOn);
+
+            if (enemyMovement!=null) {
+                int childCount = enemyMovement.transform.childCount;
+                for (int i = 0; i < childCount; i++) {
+                    Transform child = enemyMovement.transform.GetChild(i);
+                    if (child.TryGetComponent<EnemyMovement>(out var enemy)){
+                        if (isFlashlightOn == true)
+                            enemy.sightRange += 10f;
+                        else
+                            enemy.sightRange -= 10f;
+                    
+                    }
+                }
+                
+            }
+           
         }
+        
     }
 }
